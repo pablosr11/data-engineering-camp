fdf6325583c41e1a48a109993f4f90323f88c427
ps <pablosr11@gmail.com>
2024-01-24 16:49:59 +0000

    gcp bucket

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,12 @@ provider "google" {
   project = "zoomcamp"
   region  = "europe-west1"
 }
+
+resource "google_storage_bucket" "static" {
+  name          = var.bucket-name
+  location      = "EU"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+}
+
