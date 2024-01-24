273fe25c761116953b7fa8efc072c8ebfc22d78a
ps <pablosr11@gmail.com>
2024-01-24 16:50:25 +0000

    add bigquery dataset and a service account for it

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,28 @@ resource "google_storage_bucket" "static" {
   uniform_bucket_level_access = true
 }
 
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "zoomcamp_dataset"
+  friendly_name               = "zoomy"
+  description                 = "Dataset for the data-engineering course"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "psiesta.com"
+  }
+}
