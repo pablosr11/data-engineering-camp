de1e1c8928750187322f6521655507d6b37d88d2
ps <pablosr11@gmail.com>
2024-02-05 10:51:43 +0000

    retry first tf-config with filed creds

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
 provider "google" {
-  project = "zoomcamp"
+  credentials = file("zoomcamp-412215-985905e8be3a.json")
+  project = "zoomcamp-412215"
   region  = "europe-west1"
+  zone    = "europe-west1-b"
 }
 
 resource "google_storage_bucket" "static" {
@@ -11,28 +22,28 @@ resource "google_storage_bucket" "static" {
   uniform_bucket_level_access = true
 }
 
-resource "google_service_account" "bqowner" {
-  account_id = "bqowner"
-}
+# resource "google_service_account" "bqowner" {
+#   account_id = "zoomcamp-service-acc"
+# }
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id                  = "zoomcamp_dataset"
-  friendly_name               = "zoomy"
-  description                 = "Dataset for the data-engineering course"
-  location                    = "EU"
-  default_table_expiration_ms = 3600000
+# resource "google_bigquery_dataset" "dataset" {
+#   dataset_id                  = "zoomcamp_dataset"
+#   friendly_name               = "zoomy"
+#   description                 = "Dataset for the data-engineering course"
+#   location                    = "EU"
+#   default_table_expiration_ms = 3600000
 
-  labels = {
-    env = "default"
-  }
+#   labels = {
+#     env = "default"
+#   }
 
-  access {
-    role          = "OWNER"
-    user_by_email = google_service_account.bqowner.email
-  }
+#   access {
+#     role          = "OWNER"
+#     user_by_email = google_service_account.bqowner.email
+#   }
 
-  access {
-    role   = "READER"
-    domain = "psiesta.com"
-  }
-}
+#   access {
+#     role   = "READER"
+#     domain = "psiesta.com"
+#   }
+# }
