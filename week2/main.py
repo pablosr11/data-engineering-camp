341e900dce908e5763c5ebb58ffe11d6b9c07dfe
ps <pablosr11@gmail.com>
2024-02-06 17:52:31 +0000

    week2-hw1

--- week2/main.py
+++ week2/main.py
@@ -1,60 +1,41 @@
-import time
 from datetime import timedelta
 
-import httpx
-from prefect import flow, serve, task
+import pandas as pd
+from prefect import flow, task, serve
 from prefect.tasks import task_input_hash
 
 
-@task(
-    cache_key_fn=task_input_hash,
-    cache_expiration=timedelta(hours=1),
-)
-def get_url(url: str, params: dict = None):
-    response = httpx.get(url, params=params)
-    response.raise_for_status()
-    return response.json()
+@task(cache_expiration=timedelta(days=1), cache_key_fn=task_input_hash)
+def extract(url: str) -> pd.DataFrame:
+    df = pd.read_csv(url)
+    return df
 
 
-@flow
-def get_open_issues(repo_name: str, open_issues_count: int, per_page: int = 100):
-    issues = []
-    pages = range(1, -(open_issues_count // -per_page) + 1)
-    for page in pages:
-        issues.append(
-            get_url.submit(
-                f"https://api.github.com/repos/{repo_name}/issues",
-                params={"page": page, "per_page": per_page, "state": "open"},
-            )
-        )
-    return [i for p in issues for i in p.result()]
+@task(cache_expiration=timedelta(days=1), cache_key_fn=task_input_hash)
+def load(df: pd.DataFrame, path: str):
+    df = df.to_parquet(path, compression="gzip")
 
 
-@flow(retries=3, retry_delay_seconds=5, log_prints=True)
-def get_repo_info(repo_name: str):
-    url = f"https://api.github.com/repos/{repo_name}"
-    repo = get_url(url)
-    issues = get_open_issues(repo_name, repo["open_issues_count"])
-    issues_per_user = len(issues) / len(set([i["user"]["id"] for i in issues]))
-    print(f"{repo_name} repository statistics 🤓:")
-    print(f"Stars 🌠 : {repo['stargazers_count']}")
-    print(f"Forks 🍴 : {repo['forks_count']}")
-    print(f"Average open issues per user 💌 : {issues_per_user:.2f}")
+@flow(log_prints=True)
+def etl_taxi():
+    color = "green"
+    year = 2020
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
+    print(f"Downloading {url}...")
+    df = extract(url)
 
-@flow
-def slow_flow(sleep: int = 60):
-    "Sleepy flow - sleeps the provided amount of time (in seconds)."
-    time.sleep(sleep)
+    print(f"Number of rows and cols: {df.shape}")
+    print(f"Columns: {df.columns}")
+
+    # load onto GCS
+    gcs_path = f"gs://pablo-does-zoomcamp/{dataset_file}.parquet"
+    load(df, gcs_path)
+
+    return df
 
 
 if __name__ == "__main__":
-    get_repo_info_deploy = get_repo_info.to_deployment(
-        name="gh-stats-deployment",
-        tags=["testing", "tutorial"],
-        cron="0 0 * * *",
-        description="Given a GitHub repository, logs repository statistics for that repo.",
-        parameters={"repo_name": "tensorflow/tensorflow"},
-    )
-    slow_flow_deploy = slow_flow.to_deployment(name="sleeper", interval=45)
-    serve(get_repo_info_deploy, slow_flow_deploy)
+    etl_taxi()
