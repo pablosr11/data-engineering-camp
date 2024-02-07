8922bd91574139abaaa69ee29efcd94f98fcb71f
ps <pablosr11@gmail.com>
2024-02-07 16:08:09 +0000

    temp infra with stringified table in BQ

--- infra/main.tf
+++ infra/main.tf
@@ -18,6 +18,21 @@ provider "google" {
   zone        = "europe-west1-b"
 }
 
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "EU"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+
+}
+
 resource "google_storage_bucket" "static" {
   name     = var.bucket-name
   location = "EU"
@@ -42,24 +57,99 @@ resource "google_storage_bucket" "static" {
 }
 
 resource "google_bigquery_dataset" "dataset" {
-  dataset_id  = "zoomcamp_dataset"
-  location    = "EU"
-  project     = "zoomcamp-412215"
-  description = "Dataset for the data-engineering course"
+  dataset_id                  = "zoomcamp_dataset"
+  location                    = "EU"
+  project                     = "zoomcamp-412215"
+  description                 = "Dataset for the data-engineering course"
+  default_table_expiration_ms = 3600000 // 1 hour
 }
 
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
+resource "google_bigquery_table" "events" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = "events"
 
-resource "google_storage_bucket" "default" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
-  location      = "EU"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
+  schema = <<EOF
+[
+  {
+    "name": "VendorID",
+    "type": "STRING"
+  },
+  {
+    "name": "lpep_pickup_datetime",
+    "type": "STRING"
+  },
+  {
+    "name": "lpep_dropoff_datetime",
+    "type": "STRING"
+  },
+  {
+    "name": "store_and_fwd_flag",
+    "type": "STRING"
+  },
+  {
+    "name": "RatecodeID",
+    "type": "STRING"
+  },
+  {
+    "name": "PULocationID",
+    "type": "STRING"
+  },
+  {
+    "name": "DOLocationID",
+    "type": "STRING"
+  },
+  {
+    "name": "passenger_count",
+    "type": "STRING"
+  },
+  {
+    "name": "trip_distance",
+    "type": "STRING"
+  },
+  {
+    "name": "fare_amount",
+    "type": "STRING"
+  },
+  {
+    "name": "extra",
+    "type": "STRING"
+  },
+  {
+    "name": "mta_tax",
+    "type": "STRING"
+  },
+  {
+    "name": "tip_amount",
+    "type": "STRING"
+  },
+  {
+    "name": "tolls_amount",
+    "type": "STRING"
+  },
+  {
+    "name": "ehail_fee",
+    "type": "STRING"
+  },
+  {
+    "name": "improvement_surcharge",
+    "type": "STRING"
+  },
+  {
+    "name": "total_amount",
+    "type": "STRING"
+  },
+  {
+    "name": "payment_type",
+    "type": "STRING"
+  },
+  {
+    "name": "trip_type",
+    "type": "STRING"
+  },
+  {
+    "name": "congestion_surcharge",
+    "type": "STRING"
   }
-
+]
+EOF
 }
-
