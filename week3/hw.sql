99a787fde689cc735c5ad0fe65173b1304f7754a
ps <pablosr11@gmail.com>
2024-02-08 17:23:24 +0000

    rest of w3 hw - sql related questions

--- week3/hw.sql
+++ week3/hw.sql
@@ -7,3 +7,22 @@ SELECT DISTINCT(Affiliated_base_number) FROM `zoomcamp-412215.zoomcamp_dataset.n
 
 SELECT DISTINCT(Affiliated_base_number) FROM `zoomcamp-412215.zoomcamp_dataset.fhv_tripdata`
 
+-- # How many records have both a blank (null) PUlocationID and DOlocationID in the entire dataset?
+SELECT COUNT(*) FROM `zoomcamp-412215.zoomcamp_dataset.native_fhv_tripdata` WHERE PUlocationID IS NULL AND DOlocationID IS NULL
+
+
+-- # filter by pickup_datetime and order by affiliated_base_number?
+CREATE TABLE
+  `zoomcamp-412215.zoomcamp_dataset.native_part_fhv_tripdata`
+PARTITION BY
+  DATE(pickup_datetime)
+AS (
+  SELECT
+    *
+  FROM
+    `zoomcamp-412215.zoomcamp_dataset.native_fhv_tripdata`
+);
+
+-- # distinct affiliated_base_number between pickup_datetime 2019/03/01 and 2019/03/31 (inclusive).
+SELECT DISTINCT(Affiliated_base_number) FROM `zoomcamp-412215.zoomcamp_dataset.native_fhv_tripdata` WHERE DATE(pickup_datetime) >= '2019-03-01' AND DATE(pickup_datetime)<='2019-03-31'
+SELECT DISTINCT(Affiliated_base_number) FROM `zoomcamp-412215.zoomcamp_dataset.native_part_fhv_tripdata` WHERE DATE(pickup_datetime) >= '2019-03-01' AND DATE(pickup_datetime)<='2019-03-31'
