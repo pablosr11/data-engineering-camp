0d4a2e55e847572490857306c482787a04f5e0da
ps <pablosr11@gmail.com>
2024-02-09 16:37:32 +0000

    fact table merging with zone

---
+++ dbt_taxidata/models/core/fact_fhv_trips.sql
@@ -0,0 +1,25 @@
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with dim_zones as (
+    select * from {{ ref('dim_zones') }}
+    where borough != 'Unknown'
+),
+fhv_trips as (
+    select * 
+    from {{ ref('stg_fhv_tripdata') }}
+    where pickup_locationid IS NOT NULL 
+    AND dropoff_locationid IS NOT NULL
+)
+select fhv_trips.*,
+    pickup_zone.borough as pickup_borough,
+    pickup_zone.zone as pickup_zone,
+    dropoff_zone.borough as dropoff_borough,
+    dropoff_zone.zone as dropoff_zone
+from fhv_trips
+    inner join dim_zones as pickup_zone on fhv_trips.pickup_locationid = pickup_zone.locationid
+    inner join dim_zones as dropoff_zone on fhv_trips.dropoff_locationid = dropoff_zone.locationid
