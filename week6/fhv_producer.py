4f1ded2c1b8ae8de1b5ec7f44e843c230b2b24c9
ps <pablosr11@gmail.com>
2024-02-15 11:07:14 +0000

    write fhv to its own topic

--- week6/fhv_producer.py
+++ week6/fhv_producer.py
@@ -6,7 +6,7 @@ from confluent_kafka import Producer
 RIDE_TYPE = "fhv"  # green or fhv or quickstart-events
 RIDES_FILEPATH = f"/Users/ps/Downloads/{RIDE_TYPE}_tripdata_2019-01.csv"
 SLEEP_BOUNDS = (2, 6)
-TOPIC = "quickstart-events"  # f"rides_{RIDE_TYPE}"
+TOPIC = f"rides_{RIDE_TYPE}"
 
 p = Producer({"bootstrap.servers": "localhost:9092"})
 
